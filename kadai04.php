ec9a021c16fffec75aac0342a85d9b73e92c6a8b
karin <karin.arata1010@gmail.com>
2023-09-19 07:15:23 +0000

    kadai04

--- kadai04.php
+++ kadai04.php
@@ -13,14 +13,22 @@ echo "\n";
 
 function product($arr){
 $result=$arr[0];
-for($i=1; $i < count($arr); $i++){
-$result *= $arr[$i];
-}
+for($i=0; $i < count($arr); $i++){
+    $result *= $arr[$i];
+    }
 return $result;
 }
 echo product(array(1,3,5,7,9));
 echo "\n";
 
-if ($max_number<$a){
-$max_number=$a;
+
+function max_array($arr){
+  // とりあえず配列の最初の要素を一番大きい値とする
+  $max_number = $arr[0];
+  foreach($arr as $a){
+    // ここで配列の中の1番大きい値を探したい
+  return $max_number;
+    }if ($max_number<$a){
+        $max_number=$a;
+        }
 }
