6d1d4a3e08cdf9f54b457a9f8f1f68db071a4e7e
karin <karin.arata1010@gmail.com>
2023-09-20 12:10:06 +0000

    kadai04

--- kadai04.php
+++ kadai04.php
@@ -12,23 +12,25 @@ return $result;
 echo "\n";
 
 function product($arr){
-$result=$arr[0];
-for($i=0; $i < count($arr); $i++){
-    $result *= $arr[$i];
-    }
-return $result;
+  $result = 1; //かけざん
+  for($i=0; $i < count($arr); $i++){
+    //$result *= $arr[$i];
+    $result = $result * $arr[$i];
+  }
+  return $result;
 }
-echo product(array(1,3,5,7,9));
+echo product(array(9,3,5,7,1));
 echo "\n";
-
+//foreach($arr as $a){$result = $result * $a の方がわかりやすい。
 
 function max_array($arr){
   // とりあえず配列の最初の要素を一番大きい値とする
   $max_number = $arr[0];
   foreach($arr as $a){
     // ここで配列の中の1番大きい値を探したい
+    if ($max_number<$a){
+      $max_number=$a;
+    }
+  }
   return $max_number;
-    }if ($max_number<$a){
-        $max_number=$a;
-        }
 }
